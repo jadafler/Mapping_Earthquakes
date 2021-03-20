526dafb33cfcdebd2fa31f816e44f119adcfa9c9
jadafler <71464211+jadafler@users.noreply.github.com>
2021-03-19 21:40:12 -0500

    update Mapping with Polygons

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,3 +1,6 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
 // We create the tile layer that will be the background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,35 +13,44 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mmapbox/satellite-streets-v11',
+    id: 'mapbox/satellite-streets-v11',
     accessToken: API_KEY
 });
 
-// Create a base layer that holds both maps.
+// Create a base layer that holds both maps
 let baseMaps = {
-  "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  Streets: streets,
+  Satellite: satelliteStreets
 };
 
-// Create the map object with center, zoom level and default layer.
+// Create the map object with center, zoom level and default layer
 let map = L.map('mapid', {
   center: [43.7, -79.3],
   zoom: 11,
-  layers: [satelliteStreets]
+  layers: [streets]
 })
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
+//Pass our map layers into our layer control and add the layer control and add the layer control to the map
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL
 let torontoHoods = "https://raw.githubusercontent.com/jadafler/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// Create a style for the lines.
+// Create a style for the lines
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: 'blue',
+  weight: 1,
+  fillColor: 'yellow'
 }
 
-// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data
 d3.json(torontoHoods).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+    }
+}).addTo(map);
 });
